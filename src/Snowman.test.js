bf58e1378ea167789c45c2cd54f3feab7e79761b
Your Full Name <you@youremail.com>
2023-01-18 15:28:15 -0500

    finished pre-further study

--- src/Snowman.test.js
+++ src/Snowman.test.js
@@ -1,14 +1,18 @@
-import { render, fireEvent} from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Snowman from "./Snowman";
 
 //either fire event 6 times or set maxWrong to 0?
-it("should show 'you lose' msg if 6 wrong guesses", function(){
+it("should show 'you lose' msg and disable letter buttons if wrong guesses exceeded.", function () {
   const { container } = render(<Snowman maxWrong={0} />);
-  expect(container.querySelector(".Snowman-letters")).not.toBeInTheDocument();
-  expect(container.querySelector(".lose-msg")).toBeInTheDocument();
-})
 
-it("matches snapshot", function(){
+  const wrongLetter = container.querySelector("button[value='b']");
+  fireEvent.click(wrongLetter);
+
+  expect(container.querySelector(".Snowman-letters")).not.toBeInTheDocument();
+  expect(container.querySelector(".Snowman-lose-msg")).toBeInTheDocument();
+});
+
+it("matches snapshot", function () {
   const { container } = render(<Snowman />);
   expect(container).toMatchSnapshot();
-})
+});
